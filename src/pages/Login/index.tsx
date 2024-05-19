9c7889f847e2db1f738fd6ce03c306c3a6c0a6fe
@Freitas024 <viniciusfreitassilva1999@gmail.com>
2024-05-19 20:35:39 -0300

    chore: melhorando os arquivos

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,73 +1,93 @@
-import { useState, ChangeEvent } from 'react';
-import {useNavigate} from 'react-router-dom'
-import axios from 'axios';
+import { useState, ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Login() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
-  }
+  };
 
   const handleChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
-  }
+  };
 
   const fetchData = async () => {
-    const url = 'https://api.homologation.cliqdrive.com.br/auth/profile/';
+    const url = "https://api.homologation.cliqdrive.com.br/auth/profile/";
 
     try {
       const response = await axios.get(url);
       console.log(response.data);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   const handleClick = async () => {
-    const url = 'https://api.homologation.cliqdrive.com.br/auth/login/';
+    const url = "https://api.homologation.cliqdrive.com.br/auth/login/";
 
     const data = {
-      email: email, 
+      email: email,
       password: password,
     };
 
     const headers = {
       //'Authorization':`Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
-      'Accept': 'application/json;version=v1_web',
-      'Content-Type': 'application/json'
-    }
-    
+      Accept: "application/json;version=v1_web",
+      "Content-Type": "application/json",
+    };
+
     try {
-      const response = await axios.post(url, data, {headers});
-      if(response.status === 200){
-        localStorage.setItem('user_token', response.data.tokens.access);
-        navigate('/profile');
+      const response = await axios.post(url, data, { headers });
+      if (response.status === 200) {
+        localStorage.setItem("user_token", response.data.tokens.access);
+        navigate("/profile");
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
-
   return (
-    <div className='flex flex-col'>
-      <h1>B2BIT</h1>
-      <input
-        className="border-solid border-2 border-sky-500"
-        type='Email'
-        value={email}
-        onChange={handleChangeEmail}
-      />
-      <input
-        className="border-solid border-2 border-sky-500"
-        type='password'
-        value={password}
-        onChange={handleChangePassword}
-      />
-      <button disabled={!email || !password} onClick={handleClick} className='bg-blue-400 text-white'>Sign in</button>
+    <div className="grid h-screen place-items-center">
+      <div className="bg-white h-5/6 w-96 rounded-lg p-4 grid grid-rows-4 gap-4 shadow-xl items-center">
+        <h1 className="text-8xl font-bold text-[#02274F] text-center">
+          B2B<span className="text-[#FDCF00]">IT</span>
+        </h1>
+
+        <div className="flex flex-col">
+          <label className="font-bold my-4">E-mail</label>
+          <input
+            placeholder="@gmail.com"
+            className="bg-[#F1F1F1] rounded-md h-14 px-4"
+            type="Email"
+            value={email}
+            onChange={handleChangeEmail}
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label className="font-bold my-4">Password</label>
+          <input
+            placeholder="password"
+            className="bg-[#F1F1F1] rounded-md h-14 px-4"
+            type="password"
+            value={password}
+            onChange={handleChangePassword}
+          />
+        </div>
+
+        <button
+          disabled={!email || !password}
+          onClick={handleClick}
+          className="bg-[#02274F] text-white py-2 h-12 rounded-md"
+        >
+          Sign in
+        </button>
+      </div>
     </div>
   );
 }
