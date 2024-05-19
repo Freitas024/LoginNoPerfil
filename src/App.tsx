ff757c4ec380ee9d84594c65b95ae2d81645e3b9
@Freitas024 <viniciusfreitassilva1999@gmail.com>
2024-05-19 09:22:58 -0300

    chore: aprimore a arquitetura das paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './Login';
-import Perfil from './Perfil';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 export default function App() {
   return(
     <Router>
       <Routes>
         <Route path="/" element={<Login/>}/>
-        <Route path="/perfil" element={<Perfil/>}/>
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
     </Router>
   );
