83a12fb36bc60bac5b41974d8496ec003dba2187
@Freitas024 <viniciusfreitassilva1999@gmail.com>
2024-05-19 22:51:53 -0300

    chore: adicione os dados

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -42,6 +42,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(url, data, { headers });
+      console.log(response.data);
       if (response.status === 200) {
         localStorage.setItem("user_token", response.data.tokens.access);
         navigate("/profile");
@@ -53,7 +54,7 @@ export default function Login() {
 
   return (
     <div className="grid h-screen place-items-center">
-      <div className="bg-white h-5/6 w-96 rounded-lg p-4 grid grid-rows-4 gap-4 shadow-xl items-center">
+      <div className="bg-white h-5/6 w-96 rounded-2xl p-4 grid grid-rows-4 gap-4 shadow-[0_0px_24px_3px_rgba(0,0,0,0.3)] items-center">
         <h1 className="text-8xl font-bold text-[#02274F] text-center">
           B2B<span className="text-[#FDCF00]">IT</span>
         </h1>
@@ -72,7 +73,7 @@ export default function Login() {
         <div className="flex flex-col">
           <label className="font-bold my-4">Password</label>
           <input
-            placeholder="password"
+            placeholder="**************"
             className="bg-[#F1F1F1] rounded-md h-14 px-4"
             type="password"
             value={password}
@@ -83,7 +84,7 @@ export default function Login() {
         <button
           disabled={!email || !password}
           onClick={handleClick}
-          className="bg-[#02274F] text-white py-2 h-12 rounded-md"
+          className="bg-[#02274F] text-white py-2 h-12 rounded-md font-semibold"
         >
           Sign in
         </button>
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,5 @@
 import react, { useEffect } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export default function Perfil() {
@@ -9,11 +10,47 @@ export default function Perfil() {
     if (!userToken) {
       navigate("/");
     }
+
+    handleUser();
   }, []);
 
+  const handleUser = async () => {
+    const url = "https://api.homologation.cliqdrive.com.br/auth/profile/";
+
+    const headers = {
+      Authorization: `Bearer ${userToken}`,
+      Accept: "application/json;version=v1_web",
+      "Content-Type": "application/json",
+    };
+
+    try {
+      const response = await axios.get(url, { headers });
+      localStorage.setItem("user_Data", response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Perfil</h1>
+    <div className="bg-[#F1F5F9] h-screen grid place-items-center">
+      <header className="bg-white w-screen h-20 flex items-center justify-end fixed top-0 left-0">
+        <button className="bg-[#02274F] text-white py-2 w-80 h-12 rounded-md font-semibold m-12">
+          Logout
+        </button>
+      </header>
+      <div className="bg-white w-1/5 h-3/6 p-12 grid grid-rows-4 shadow-[0_0px_12px_0px_rgba(0,0,0,0.1)] rounded-xl">
+        <h1 className="font-normal text-center text-center">profile picture</h1>
+        <h3 className="text-center"></h3>
+        <label className="m-4">
+          Your <span className="font-bold">Name</span>
+        </label>
+        <h2 className="bg-[#F1F1F1] rounded-md px-2 h-8">name</h2>
+        <label className="m-4">
+          Your<span className="font-bold">E-mail</span>
+        </label>
+        <h2 className="bg-[#F1F1F1] rounded-md px-2 h-8">Email</h2>
+      </div>
     </div>
   );
 }
